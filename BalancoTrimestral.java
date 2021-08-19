d42b1ea7e3d5907161cba39882da8d84ef52d465
carlam97 <carmartins04@hotmail.com>
2021-08-19 17:44:58 +0000

    Println

--- BalancoTrimestral.java
+++ BalancoTrimestral.java
@@ -1,3 +1,11 @@
 public class BalancoTrimestral{
-    
+    public static void main(String args[]){
+        int gastosJaneiro=15000;
+        int gastosFevereiro=23000;
+        int gastosMarco=17000;
+        int gastosTrimestre= gastosJaneiro + gastosFevereiro + gastosMarco;
+        int gastosMensal= gastosTrimestre/3;
+        System.out.println("O total do gasto Trimestral foi de " + gastosTrimestre);
+        System.out.println("Sendo assim a média do gasto mensal foi de " + gastosMensal);
+    }
 }
